b807bf550ac47f7b8bfceb7b29a309de2fda0c78
Mikaela Gurney <354@holbertonschool.com>
2018-12-08 13:37:31 -0800

    Fix auto-unchecking

--- src/components/ui/Color.js
+++ src/components/ui/Color.js
@@ -10,19 +10,30 @@ const Color = ({
   onRate = f => f,
   onRemove = f => f,
   toggleSelect = f => f
-}) => (
-  <section className={"color-card" + (selected ? " selected" : "")}>
-    <h1 className="heading heading-md light u-mt-sm">{title}</h1>
-    <input type="checkbox" label="color-select" onClick={toggleSelect} />
-    <div className="color-card__btn-container">
-      <button className="color-card__btn-remove" onClick={onRemove}>
-        &times;
-      </button>
-    </div>
-    <div className="color-card__color" style={{ backgroundColor: color }} />
-    <StarRating starsSelected={rating} onRate={onRate} />
-  </section>
-);
+}) => {
+  //Call toggle action and
+  const onClick = () => toggleSelect;
+
+  return (
+    <section className={"color-card" + (selected ? " selected" : "")}>
+      <input
+        type="checkbox"
+        label="color-select"
+        className="color-card__select"
+        checked={selected ? "checked" : false}
+        onClick={toggleSelect}
+      />
+      <div className="color-card__btn-container">
+        <button className="color-card__btn-remove" onClick={onRemove}>
+          &times;
+        </button>
+      </div>
+      <h1 className="heading heading-md light u-mt-sm">{title}</h1>
+      <div className="color-card__color" style={{ backgroundColor: color }} />
+      <StarRating starsSelected={rating} onRate={onRate} />
+    </section>
+  );
+};
 
 Color.propTypes = {
   title: PropTypes.string.isRequired,
